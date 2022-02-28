a11d6e5e6d973a885dd8a1c7791f08da6c73fa59
Ramiro Morales <cramm0@gmail.com>
2022-02-28 12:16:49 -0300

    just the failing test case code

--- tests/postgres_tests/test_bulk_update.py
+++ tests/postgres_tests/test_bulk_update.py
@@ -1,39 +1,31 @@
-from datetime import date
-
-from django.db import connection, transaction
 from django.test import modify_settings
 
 from . import PostgreSQLTestCase
-from .models import HStoreModel, OtherTypesArrayModel, RangesModel
+from .models import HStoreModel
 
 
 @modify_settings(INSTALLED_APPS={"append": "django.contrib.postgres"})
 class BulkSaveTests(PostgreSQLTestCase):
     def test_bulk_update(self):
-        if connection.psycopg_version[0] < 3:
-            from psycopg2.extras import DateRange, NumericRange
-        else:
-            from psycopg.types.range import Range
-
-            DateRange = NumericRange = Range
 
         test_data = [
             # (IntegerArrayModel, "field", [], [1, 2, 3]),
             # (NullableIntegerArrayModel, "field", [1, 2, 3], None),
             # (NestedIntegerArrayModel, "field", [], [[1, 2, 3]]),
             (HStoreModel, "field", {}, {1: 2}),
-            (RangesModel, "ints", None, NumericRange(lower=1, upper=10)),
-            (
-                RangesModel,
-                "dates",
-                None,
-                DateRange(lower=date.today(), upper=date.today()),
-            ),
-            (OtherTypesArrayModel, "ips", [], ["1.2.3.4"]),
-            (OtherTypesArrayModel, "json", [], [{"a": "b"}]),
+            # (RangesModel, "ints", None, NumericRange(lower=1, upper=10)),
+            # (
+            #     RangesModel,
+            #     "dates",
+            #     None,
+            #     DateRange(lower=date.today(), upper=date.today()),
+            # ),
+            # (OtherTypesArrayModel, "ips", [], ["1.2.3.4"]),
+            # (OtherTypesArrayModel, "json", [], [{"a": "b"}]),
         ]
         for Model, field, initial, new in test_data:
-            with self.subTest(model=Model, field=field), transaction.atomic():
+            with self.subTest(model=Model, field=field):
+                # with self.subTest(model=Model, field=field), transaction.atomic():
                 instances = Model.objects.bulk_create(
                     Model(**{field: initial}) for _ in range(20)
                 )
